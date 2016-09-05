6cb9b9afd90956cb599318a641fd03c7424be536
Calum Chamberlain work <calum.chamberlain@vuw.ac.nz>
2016-09-05 04:21:10 +0000

    Add needed imports for flake8 tests

--- obspy/io/nordic/core.py
+++ obspy/io/nordic/core.py
@@ -15,10 +15,9 @@ the two formats.
     information) with the pick.resource_id (where the pick contains only \
     physical measured information).
 """
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from future.builtins import *  # NOQA @UnusedWildImport
 from six import string_types
 from obspy import UTCDateTime
 import numpy as np
--- obspy/io/nordic/tests/test_nordic.py
+++ obspy/io/nordic/tests/test_nordic.py
@@ -1,10 +1,9 @@
 """
 Functions for testing the utils.sfile_util functions
 """
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+from future.builtins import *  # NOQA @UnusedWildImport
 from obspy.io.nordic.core import *
 from obspy.io.nordic.core import _int_conv, _evmagtonor, _float_conv, \
     _nortoevmag, _str_conv
